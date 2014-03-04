f3f91a318df586f066e83bbedbc1d19c6412dd1b
Angel Preciado <strikeagle15@comcast.net>
2014-03-04 12:47:05 -0700

    initialized netbeans project and repository

---
+++ NeuralNetworkProject/src/neuralnetworkproject/NeuralNetworkProject.java
@@ -0,0 +1,24 @@
+/*
+ * An implementation of a Neural Network with the objective of teaching
+ * the network to recognize patterns in images.
+ *  
+ *  Bio Computing
+ *  March 4th, 2014
+ */
+
+package neuralnetworkproject;
+
+/**
+ *
+ * @author Angel Preciado, Colby Sinnock, Alex Mohr
+ */
+public class NeuralNetworkProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
